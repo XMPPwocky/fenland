6ca0dba1390d31703701155a21e41f710c701b90
wocky <wocky@xmppwocky.net>
2012-05-20 20:44:09 -0700

    Begin generating translation tables

--- src/kernel/vm.s
+++ src/kernel/vm.s
@@ -10,4 +10,25 @@ VIRTUAL_ADDRESS	= 0x80008000
  * r0 = size of RAM
  * r1 = physical address of start of RAM */
 setup_mmu:
+	/* save arguments */
+	mov	r4, r0
+	mov	r5, r1
+
+	/* Determine offset from where we were loaded to our virtual address */
+address_anchor:
+	ldr	r0, =address_anchor	/* Virtual address of address_anchor */
+	adr	r1, address_anchor	/* Physical address of address_anchor */
+	subs	r6, r0, r1 /* offset */
+
+	beq	$$no_relocation /* VA = PA */
+	bkpt
+$$no_relocation:
+
+	/* Okay, now: do we have more than 2GB of RAM?
+	 * If so, we can't map it all into the higher half. */
+
+	cmp	r0, #0x80000000
+	beq	$$can_map_all_ram
+	bkpt
+$$can_map_all_ram:
 	bkpt
